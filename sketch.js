8a544c7e2947573744ece0d00562ad3d72d0e274
QuirkyBoy <160971272+QuirkyBoy@users.noreply.github.com>
2024-05-30 23:21:29 +0000

    initial commit

--- sketch.js
+++ sketch.js
@@ -1,137 +1,143 @@
-let rocket, astroid, rightWall, topWall, leftWall, bottomWall, exit, spaceImage
+let rocket, astroids, rightWall, topWall, leftWall, bottomWall, exit, instructions, levelKeeper, liveKeeper, shipImage, bgImage, astroidImage, coinEffect, crashEffect, sadTromboneEffect, level, lives, noAstroids;
 
-level = 1
-lives = 3
-noAstroids = 300
+level = 1; //sets level variable to 1
+lives = 4; //sets lives variable to 4
+noAstroids = 200; //sets the number (no) of astroids to 200
 
 function preload(){
-  spaceImage = loadImage("Planets.jpg");
-  spaceship = loadImage("Spaceship.png");
+  soundFormats("mp3");
+  theme = loadSound("theme.mp3")
+  coinEffect = loadSound("mario_coin_sound.mp3");
+  crashEffect = loadSound("glass_effect.mp3");
+
+  planet = loadImage("planet.jpg");
 }
 
 function setup(){
-	new Canvas();
+  new Canvas();
 
-	astroids = new Group();
-	rocket = new Sprite(40, 300, 40, "triangle");
-	rocket.img = "Spaceship.png";
-  rocket.diameter = 1;
-	rocket.stroke = 'white'
-  rocket.layer = 1
-	
-  exit = new Sprite(width-20, height/2, 20, 200, "static");
-	exit.color = "cyan";
+  theme.play();
 
-	rightWall = new Sprite(width, height/2, 15, height, "static");
-	rightWall.color = "red";
+  astroids = new Group(); //creates group 'astroids'
 
-	topWall = new Sprite(width/2, height, width, 15, "static");
-	topWall.color = "red";
+  rocket = new Sprite(40, 300, 29, 11); //creates rocket
+  rocket.img = "spaceship.png";
+  rocket.layer = 1;
 
-	leftWall = new Sprite(width/width, height/2, 15, height, "static");
-	leftWall.color = "red";
+  exit = new Sprite(width-20, height/2, 20, 200, "static"); //creates exit for rocket
+  exit.color = "cyan";
 
-	bottomWall = new Sprite(width/2, height/height, width, 15, "static");
-	bottomWall.color = "red";
+  rightWall = new Sprite(width, height/2, 15, height, "static"); //creates the right wall
+  rightWall.color = "red";
 
-	levelKeeper = new Sprite(width-40, 40, 60, 60, "static");
-	levelKeeper.color = "red";
-  liveKeeper = new Sprite(width-105, 40, 60, 60, "static");
+  topWall = new Sprite(width/2, height, width, 15, "static"); //creates the top wall
+  topWall.color = "red";
+
+  leftWall = new Sprite(width/width, height/2, 15, height, "static"); //creates the left wall
+  leftWall.color = "red";
+
+  bottomWall = new Sprite(width/2, height/height, width, 15, "static"); //creates the bottom wall
+  bottomWall.color = "red";
+
+  levelKeeper = new Sprite(width-40, 40, 60, 60, "static"); //creates the sprite to display the level
+  levelKeeper.color = "red";
+
+  liveKeeper = new Sprite(width-105, 40, 60, 60, "static"); //creates the sprite to display lives
   liveKeeper.color = "red";
-  
-	for (var i = 0;i<noAstroids;i++){
-        astroid = new astroids.Sprite(random(100, width-100), random(height), 10, "triangle");
-        astroid.color = "grey";
-        astroid.direction = random(360);
-        astroid.speed = 0.1;
-    	  astroid.bounciness = 0.8;
-        astroid.layer = 1
-        astroids.stroke = "black";
-  }
-    //moved the instruction block down after the rockets and astroids MrsJ
-    instructions = new Sprite(width/2, height/2, width, height, "static");
-    instructions.color = "black";
-    instructions.textColor = "red";
-    instructions.textSize = 45;
-    instructions.text = "The rocket propels itself towards your mouse *on each click* \n The aim is to reach the blue exit at the other side of the asteroid field \n If you hit an asteroid or a wall you'll lose a life, if you lose all 3 it's game over";
-    instructions.life = 100;
-    instructions.layer = 100;
-    instructions.overlaps(astroids) //added MrsJ
-    instructions.overlaps(rocket) //added MrsJ
-}//end setup function
 
+  instructions = new Sprite(width/2, height/2, width, height, "static"); //creates the sprite to display instructions
+  instructions.color = "black";
+  instructions.textColor = "red";
+  instructions.textSize = 45;
+  instructions.text = "The rocket propels itself towards your mouse *on each click* \n The aim is to reach the blue exit at the other side of the asteroid field \n If you hit an asteroid or a wall you'll lose a life, if you lose all 3 it's game over \n \n Level one is a practice, then it gets harder \n \n **DON'T CLICK UNTIL GAME STARTS**";
+  instructions.life = 580; //removes instructions after n seconds
+  instructions.layer = 100;
+  instructions.overlaps(astroids); //makes it go over the other sprites to not affect gameplay
+  instructions.overlaps(rocket); // ^ ^ ^ ^
+
+  for (var i = 0;i<noAstroids;i++){
+    astroid = new astroids.Sprite(random(100, width-100), random(height), 10, "triangle"); //creates n astroids between inserted coords
+    astroid.img = "astroids.png";
+    astroid.direction = random(360);
+    astroid.speed = 0.05;
+    astroid.bounciness = 0.8;
+    astroid.layer = 1;
+  } //end create astroids if
+} //end setup function
+
+function endGame(){
+  console.log("game ending..."); //sends a message to the console
+  gameOver = new Sprite(width/2, height/2, width, height, "static"); //creates a sprite to display endgame text
+  gameOver.color = "red";
+  gameOver.text = "GAME OVER - You made it to level " + level + "\n \n Press F5 to try and better your score \n \n Pls like my game";
+  gameOver.textSize = 45;
+} //end game over function
 
 function draw() {
-      background(10);
-  
-      imageMode(CENTER);
-      image(spaceImage, width/2, height/2, width, height);
+  background(255);
 
-      rocket.rotationSpeed = 0;
-  
-    	levelKeeper.textSize = 20;
-    	levelKeeper.textColor = "blue";
-    	levelKeeper.text = "Level:\n" + level;
-      liveKeeper.textSize = 20;
-      liveKeeper.textColor = "blue";
-      liveKeeper.text = "Lives:\n" + lives;
+  imageMode(CENTER); //centers image
+  image(planet, width/2, height/2, width, height); //inserts and formats background image
 
-    	if (rocket.collides(exit)){
-    		console.log("hit");
-    		level += 1;
-    		rocket.remove();
-    		astroids.remove();
-        
-        rocket = new Sprite(40, 300, 40, "triangle");
-        rocket.color = "red";
-        rocket.stroke = 'white';
-        rocket.layer = 1;
+  astroids.rotationSpeed = 0; //stops sprites from rotating crazily
+  rocket.rotationSpeed = 0; // ^ ^ ^ ^
 
-        for (var i = 0;i<noAstroids + 75;i++){
-          astroid = new astroids.Sprite(random(100, width-100), random(height), 10, "triangle");
-          astroid.color = "grey";
-          astroid.direction = random(360);
-          astroid.speed = 0.1;
-          astroid.bounciness = 0.8;
-          astroid.layer = 1
-          astroid.stroke = "black";
-        }
-    	}
-    
-    	if (rocket.collides(astroids)){
-    		console.log("miss");
-    		lives -= 1;
-    	}else if (rocket.collides(leftWall)){
-    		console.log("miss");
-    		lives -= 1;
-    	}else if (rocket.collides(rightWall)){
-    		console.log("miss");
-    		lives -= 1;
-    	}else if (rocket.collides(topWall)){
-    		console.log("miss");
-    		lives -= 1;
-    	}else if (rocket.collides(bottomWall)){
-    		console.log("miss");
-    		lives -= 1;
-    	}
-    
-  function endGame(){
-      console.log("game ending...")
-      astroids.remove()
-      rocket.remove()
-      gameOver = new Sprite(width/2, height/2, width, height, "static");
-      gameOver.text = "GAME OVER - You made it to level " + level + "\n \n Press F5 to try and better your score";
-      gameOver.textSize = 45;
-      gameOver.color = "red";
-  }
-  
+  levelKeeper.textSize = 20;
+  levelKeeper.textColor = "blue";
+  levelKeeper.text = "Level:\n" + level; //displays level
+  liveKeeper.textSize = 20;
+  liveKeeper.textColor = "blue";
+  liveKeeper.text = "Lives:\n" + lives; //displays lives
+
+  if(mouse.presses()){
+    rocket.moveTowards(mouse.x, mouse.y, 0.02); //tells rocket to move towards mouse at 0.02 speed when mouse is clicked
+  }//end rocket movement
+
+  if (rocket.collides(exit)){
+    console.log("hit") //sends a message to console
+    rocket.remove(); //removes rocket ready for reset
+    astroids.remove(); // ^ ^ ^ ^
+    coinEffect.play(); //plays sound effect
+    level += 1; //increases level by 1
+
+    rocket = new Sprite(40, 300, 29, 11); //remakes rocket
+    rocket.img = "spaceship.png";
+    rocket.layer = 1;
+
+    for (var i = 0;i<noAstroids + 100;i++){
+      astroid = new astroids.Sprite(random(100, width-100), random(height), 10, "triangle");
+      astroid.img = "astroids.png";
+      astroid.direction = random(360);
+      astroid.speed = 0.1;
+      astroid.bounciness = 0.8;
+      astroid.layer = 1
+      astroid.stroke = "black";
+    } //end create astroids
+
+  }else if (rocket.collides(astroids)){
+    console.log("miss");
+    lives -= 1;
+    crashEffect.play(); //plays effect if you hit something
+  }else if (rocket.collides(leftWall)){
+    console.log("miss");
+    lives -= 1;
+    crashEffect.play();
+  }else if (rocket.collides(rightWall)){
+    console.log("miss");
+    lives -= 1;
+    crashEffect.play();
+  }else if (rocket.collides(topWall)){
+    console.log("miss");
+    lives -= 1;
+    crashEffect.play();
+  }else if (rocket.collides(bottomWall)){
+    console.log("miss");
+    lives -= 1;
+    crashEffect.play();
+  } //end if
+
   if (lives == 0){
-    	console.log("Game_Over");
-      endGame()
-    }//end game over if statement
-}//end draw function
-
-function mouseClicked(){
-  rocket.moveTowards(mouse.x, mouse.y, 0.01);
-}
-
+    console.log("Game_Over");
+    endGame();
+  }//end game over if
+}//end draw
